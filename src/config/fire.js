7151e25ee12e0359e6bdd717fd6a80cd5f69037a
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-13 20:57:35 +0700

    add commit

---
+++ src/config/fire.js
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBev4Fo0jRygxGEizvKWG8kN2UI5GE7RBY",
+    authDomain: "mini-project-f77ac.firebaseapp.com",
+    databaseURL: "https://mini-project-f77ac.firebaseio.com",
+    projectId: "mini-project-f77ac",
+    storageBucket: "mini-project-f77ac.appspot.com",
+    messagingSenderId: "596162951065",
+    appId: "1:596162951065:web:22c5044c93afb1a4938508",
+    measurementId: "G-S1FCYZB59H"
+    
+  };
